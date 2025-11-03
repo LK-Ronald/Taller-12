320b071849cc6396db29ce52fd25ce7da1f92305
LkRonald <rpereirac@unicartagena.co>
2025-11-03 12:14:37 -0500

    feat: se agrego la clase Main con la instancia de Persona

---
+++ Ejercicio1/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona persona = new Persona("Ronald");
+        persona.trabajar();
+        persona.hablar();
+
+    }
+}
