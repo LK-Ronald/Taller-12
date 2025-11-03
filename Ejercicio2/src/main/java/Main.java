5d439a6beae09c8302c7ec81d157e08c0bbb8943
LkRonald <rpereirac@unicartagena.co>
2025-11-03 12:54:10 -0500

    feat: se agrego la clase Main con las instancias de Animal

---
+++ Ejercicio2/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal pato = new Animal("Pato");
+        pato.nadar();
+        pato.volar();
+
+        Animal gallina  = new Animal("Gallina");
+        gallina.nadar();
+        gallina.volar();
+    }
+}
